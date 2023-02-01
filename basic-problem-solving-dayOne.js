64eaf3e27ac92538e248c58e45d23b1b45a0a699
Nissan1998 <jnnissanm@gmail.com>
2023-02-01 10:07:54 +0600

    harryProblem

--- basic-problem-solving-dayOne.js
+++ basic-problem-solving-dayOne.js
@@ -1,3 +1,8 @@
-var nissan={
-    boy: 'honest',
-}
+// 1st problem solving Harry's mom gave tk to buy fruits:-
+let givenTk = 1000;
+let buyAppleTk = 400;
+let buyOrangeTk = 300;
+let totalExpense = buyAppleTk + buyOrangeTk;
+let shopperGivenTk = givenTk - totalExpense;
+console.log(shopperGivenTk);
+
