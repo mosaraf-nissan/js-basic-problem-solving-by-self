05b995297afde711a8829d265c8355831efa43fb
Nissan1998 <jnnissanm@gmail.com>
2023-02-01 10:39:16 +0600

    solvedP3

---
+++ basic-problem-solving3.js
@@ -0,0 +1,21 @@
+/*
+    John’s teacher gave him two variables. Each variable contains a string. John’s teacher asked him to
+    combine these two strings and print them in one line. Help John write the program.
+    
+    Input:
+        The first and the second lines of the input contain the strings.
+    
+    Output:
+        Print the result in one line.
+        Practice Problem 3
+
+    Sample Input:
+        I am going to be
+        an awesome web developer
+    Output:
+        I am going to be an awesome web developer
+*/
+let teacher1sString = 'I am going to be ';
+let teacher2ndString = 'an awesome web dveloper';
+let teacherSaid = teacher1sString+teacher2ndString;
+console.log(teacherSaid);
