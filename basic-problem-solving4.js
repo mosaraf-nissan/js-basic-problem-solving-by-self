0b2cab32ab4c7b6e84ebdb504cce306317e3ade8
Nissan1998 <jnnissanm@gmail.com>
2023-02-01 10:53:31 +0600

    solved04

---
+++ basic-problem-solving4.js
@@ -0,0 +1,17 @@
+/*Sarah’s mother is teaching her mathematics. She gave Sarah a number and asked her what the
+remainder would be if she divided the number by 5. Help Sarah write the program.
+Input:
+The first line of the input contains the number.
+Output:
+Print the remainder.
+
+Practice Problem 4
+
+Sample Input:
+119
+
+Output:
+4*/
+let mathematicsNumber = 119;
+let remainder = 119 % 5;
+console.log(remainder);
